59c5c762c7fc42016355de6eed5c5184076b53bb
orangeem <orangeem@naver.com>
2016-02-16 13:52:36 -0500

    Fix Hero movement

--- Assets/_Scripts/HeroController.cs
+++ Assets/_Scripts/HeroController.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class HeroController : MonoBehaviour {
-	//Private Instance Variables
+
+	// PRIVATE  INSTANCE VARIABLES
 	private Animator _animator;
 	private float _move;
 	private float _jump;
@@ -16,42 +17,40 @@ public class HeroController : MonoBehaviour {
 		this._animator = gameObject.GetComponent<Animator> ();
 		this._move = 0f;
 		this._jump = 0f;
-		this._facingRight = true;	
+		this._facingRight = true;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		this._move = Input.GetAxis ("Horizontal");
 		this._jump = Input.GetAxis ("Vertical");
-
+		Debug.Log (this._jump);
 		if (this._move != 0) {
 			if (this._move > 0) {
 				this._facingRight = true;
 				this._flip ();
 			}
-
 			if (this._move < 0) {
 				this._facingRight = false;
 				this._flip ();
 			}
-			//call the walk clip
+
+			// call the walk clip
 			this._animator.SetInteger ("AnimState", 1);
 		} else {
-			//set default animation state to "idle"
-			this._animator.SetInteger("AnimState", 0);
+
+			// set default animation state to "idle"
+			this._animator.SetInteger ("AnimState", 0);
 		}
 
 		if (this._jump > 0) {
-			//call the jump animation state to "idle"
-			this._animator.SetInteger("AnimState", 2);
+			// call the "jump" clip
+			this._animator.SetInteger ("AnimState", 2);
 		}
-
-
-	
 	}
 
-	//Private Methods
-	private void _flip(){
+	// PRIVATE METHODS
+	private void _flip() {
 		if (this._facingRight) {
 			this._transform.localScale = new Vector2 (1, 1);
 		} else {
