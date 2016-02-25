6e1336b0259921ee9cfef57b8e80715f16590b51
orangeem <orangeem@naver.com>
2016-02-25 04:50:54 -0500

    Chage hero img. Add light, sound, collider

--- Assets/_Scripts/HeroController.cs
+++ Assets/_Scripts/HeroController.cs
@@ -19,9 +19,11 @@ public class VelocityRange{
 public class HeroController : MonoBehaviour {
 	//Public Instance Variables
 	public VelocityRange velocityRange;
-	public float moveForce;
-	public float jumpForce;
+	public float moveForce=1000;
+	public float jumpForce=20000;
 	public Transform groundCheck;
+	public Transform camera;
+	public GameController gameController;
 
 	// PRIVATE  INSTANCE VARIABLES
 	private Animator _animator;
@@ -31,6 +33,9 @@ public class HeroController : MonoBehaviour {
 	private Transform _transform;
 	private Rigidbody2D _rigidBody2d;
 	private bool _isGrounded;
+	private AudioSource[] _audioSources;
+	private AudioSource _jumpSound;
+	private AudioSource _lightSound;
 
 	// Use this for initialization
 	void Start () {
@@ -44,10 +49,24 @@ public class HeroController : MonoBehaviour {
 		this._jump = 0f;
 		this._move = 0f;
 		this._facingRight = true;
+
+		// Setup AudioSources
+		this._audioSources = gameObject.GetComponents<AudioSource>();
+		this._jumpSound = this._audioSources [0];
+		this._lightSound = this._audioSources [1];
+
+
+
+		// place the hero in the starting position
+		this._spawn ();
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
+
+		Vector3 currentPosition = new Vector3 (this._transform.position.x, this._transform.position.y, -10f);
+		this.camera.position = currentPosition;
+
 		this._isGrounded = Physics2D.Linecast (this._transform.position, 
 			this.groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
 		Debug.DrawLine (this._transform.position, this.groundCheck.position);
@@ -66,7 +85,7 @@ public class HeroController : MonoBehaviour {
 			this._move = Input.GetAxis ("Horizontal");
 			this._jump = Input.GetAxis ("Vertical");
 
-			
+
 			if (this._move != 0) {
 				if (this._move > 0) {
 					//movement force
@@ -95,6 +114,7 @@ public class HeroController : MonoBehaviour {
 			if (this._jump > 0) {
 				//jump force
 				if (absVelY < this.velocityRange.maximum) {
+					this._jumpSound.Play ();
 					forceY = this.jumpForce;
 				}
 
@@ -108,6 +128,19 @@ public class HeroController : MonoBehaviour {
 		this._rigidBody2d.AddForce(new Vector2(forceX, forceY));
 	}
 
+	void OnCollisionEnter2D(Collision2D other){
+
+		if(other.gameObject.CompareTag("Light")){
+			this._lightSound.Play ();
+			Destroy (other.gameObject);
+			this.gameController.ScoreValue += 20;
+		}
+
+		if(other.gameObject.CompareTag("Death")){
+			this._spawn ();
+			this.gameController.LivesValue--;
+		}
+	}
 	// PRIVATE METHODS
 	private void _flip() {
 		if (this._facingRight) {
@@ -115,5 +148,9 @@ public class HeroController : MonoBehaviour {
 		} else {
 			this._transform.localScale = new Vector2 (-1, 1);
 		}
+	}
+
+	private void _spawn(){
+		this._transform.position = new Vector3 (-263.4f, 156f, 0);
 	}
 }
